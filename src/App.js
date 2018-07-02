d9cc76d6a2a443dba4134aab59b7966a9d474272
Roger Diaz <roger.diaz@rivetlogic.com>
2018-07-02 12:24:38 -0600

    Add Banner

--- src/App.js
+++ src/App.js
@@ -1,18 +1,75 @@
 import React, { Component } from 'react';
+import { FormGroup, FormControl, ControlLabel, Table } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleNetworkChange = this.handleNetworkChange.bind(this);
+    this.handleHostsChange = this.handleHostsChange.bind(this);
+
+    this.state = {
+      network: '',
+      hosts: ''
+    };
+  }
+
+  handleNetworkChange(e) {
+    this.setState({ network: e.target.value });
+  }
+
+  handleHostsChange(e) {
+    this.setState({ hosts: e.target.value });
+  }
+
   render() {
+    let {network, hosts} = this.state;
+    let _hosts = hosts.split(",")
+    .sort((a,b) => a > b)
+    .map((h) => 
+      <tr>
+        <td>{h}</td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    );
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Subnet Calc v1</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className="container">
+          <form>
+            <FormGroup>
+              <ControlLabel>Start Network: </ControlLabel>
+              <FormControl type="text" value={network} placeholder="Start Network" onChange={this.handleNetworkChange} />
+            </FormGroup>
+            <FormGroup>
+              <ControlLabel>Host List: </ControlLabel>
+              <FormControl type="text" value={hosts} placeholder="eg: 200,10,1000,2,1" onChange={this.handleHostsChange} />
+            </FormGroup>
+          </form>
+          <hr />
+          
+          <Table>
+            <thead>
+              <tr>
+                <th>Host</th>
+                <th>Mask</th>
+                <th>Network</th>
+                <th>Broadcast</th>
+              </tr>
+            </thead>
+            <tbody>
+              {_hosts}
+            </tbody>
+          </Table>
+        </div>
+        
       </div>
     );
   }
